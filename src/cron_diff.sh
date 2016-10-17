23f5831582014441c309dc9c680150e781484326
Murthy10 <samuel.kurath@gmail.com>
2016-10-17 16:01:55 +0200

    changed diff

--- src/cron_diff.sh
+++ src/cron_diff.sh
@@ -1,20 +1,10 @@
 #! /bin/bash
 
 SEQ=$(curl https://overpass-api.de/api/augmented_diff_status)
-
-MM=$[$SEQ/1000000]
-KK=$[$SEQ-$MM*1000000]
-KK=$[$KK/1000]
-UU=$[$SEQ-$MM*1000000-$KK*1000]
-A=`printf %03d $MM`
-B=`printf %03d $KK`
-C=`printf %03d $UU`
-
-echo $A/$B/$C
-
 file="/sequence.txt"
 old_sequence=$(cat "$file")
 
 if [ "$old_sequence" != "$SEQ" ]; then
-    curl https://overpass-api.de/api/augmented_diff | /opt/OSMstream/producer.py
+    curl https://overpass-api.de/api/augmented_diff?id=$SEQ | tac | tac | /opt/OSMstream/producer.py
+    echo $SEQ > $file
 fi
