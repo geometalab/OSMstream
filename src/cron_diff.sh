2fb894eff0367e229946cdbe535c4139e2fb07bd
Murthy10 <samuel.kurath@gmail.com>
2016-10-17 10:53:18 +0200

    fixed long stdin

--- src/cron_diff.sh
+++ src/cron_diff.sh
@@ -5,6 +5,6 @@ file="/sequence.txt"
 old_sequence=$(cat "$file")
 
 if [ "$old_sequence" != "$SEQ" ]; then
-    curl https://overpass-api.de/api/augmented_diff?id=$SEQ  | /opt/OSMstream/producer.py
+    curl https://overpass-api.de/api/augmented_diff?id=$SEQ | tac | tac | /opt/OSMstream/producer.py
     echo $SEQ > $file
 fi
